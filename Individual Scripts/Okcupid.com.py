9f9163d219000237276894cf7c32d7a5301f0173
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:46:24 -0400

    Update Okcupid.com.py

--- Individual Scripts/Okcupid.com.py
+++ Individual Scripts/Okcupid.com.py
@@ -4,19 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in Okcupid are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in ["superman@gmail.com", "priyasdsdanka@gmail.com", "batman@gmail.com", "asjdbkajsdnkjas@gmail.com", "spiderman@gmail.com"]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.okcupid.com/signup")
     p1 = driver.page_source
@@ -32,4 +32,4 @@ for i in ["superman@gmail.com", "priyasdsdanka@gmail.com", "batman@gmail.com", "
     if "That email is invalid or in use" in p3:
         print(i)
     sleep(2)
-driver.close()
+    driver.close()
