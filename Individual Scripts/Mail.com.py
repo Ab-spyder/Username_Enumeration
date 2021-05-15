b831bdab2f5380a02b69022ef7dd6e97eb80b836
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:46:16 -0400

    Update Mail.com.py

--- Individual Scripts/Mail.com.py
+++ Individual Scripts/Mail.com.py
@@ -4,19 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in Mail.com are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in ["BegForMercy", "BraiasjdnkjnAxe", "CrazyMind", "DeathWaasdaish", "DisasterMaster", "ElNaaaino", "EndlessFacepalms" ]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://signup.mail.com/#.1272-header-signup2-1")
     sleep(3)
@@ -29,4 +29,4 @@ for i in ["BegForMercy", "BraiasjdnkjnAxe", "CrazyMind", "DeathWaasdaish", "Disa
     p3 = extract_html_diff.as_string(p2, p1)
     if "This choice of email address has already been assigned" in p3:
        print(i)
-driver.close()
+    driver.close()
