c9839852636e16db699e0c80a1aa5cde0c77b5ad
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:36:16 -0400

    Update Amazon.py

--- Individual Scripts/Amazon.py
+++ Individual Scripts/Amazon.py
@@ -7,12 +7,6 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
 
 print("Usernames present in amazon are: ")
 
@@ -20,6 +14,13 @@ print("Usernames present in amazon are: ")
 # You can remove all the Email IDs below
 for i in ["ccohen@gmail.com", "asdbaskjdbasdba@gmail.com", "superman@gmail.com", "keijser@yahoo.com", "maryjane@gmail.com", "ajohnson@yahoo.com", "rnewman@gmail.com", "sfoskett@yahoo.com", "seurat@yahoo.com", "richard@gmail.com", "punkis@gmail.com"]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&")
     p1 = driver.page_source
@@ -32,4 +33,4 @@ for i in ["ccohen@gmail.com", "asdbaskjdbasdba@gmail.com", "superman@gmail.com",
     if "Change" in p3:
        print(i)
     sleep(3)
-driver.close()
+    driver.close()
