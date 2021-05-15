fc742ab924dc9603468d9f64807835fd294f3bc7
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:39:45 -0400

    Update Deloitte.com.py

--- Individual Scripts/Deloitte.com.py
+++ Individual Scripts/Deloitte.com.py
@@ -3,19 +3,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in Deloitte are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in ["superman@gmail.com", "yenojam209@shzsedu.com", "priyasdsdanka@gmail.com", "yenojam209@shzsedu.com", "byenojam209@shzsedu.com", "asjdbkajsdnkjas@gmail.com", "spiderman@gmail.com", "yenojam209@shzsedu.com"]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://my.deloitte.com/?site=us-en")
     p1 = driver.page_source
@@ -30,4 +30,4 @@ for i in ["superman@gmail.com", "yenojam209@shzsedu.com", "priyasdsdanka@gmail.c
     if "There's already an account associated with this email address." in p3:
         print(i)
     sleep(5)
-driver.close()
+    driver.close()
