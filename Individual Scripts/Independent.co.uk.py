1ef243330c252e0f37e46e6212c4656937beb314
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:46:14 -0400

    Update Independent.co.uk.py

--- Individual Scripts/Independent.co.uk.py
+++ Individual Scripts/Independent.co.uk.py
@@ -4,19 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in independent.co.uk are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in ["supeasdasdrman@gmail.com", "dahopi6913@dwgtcm.com", "priyanka@gmail.com", "batman@gmail.com", "michael@gmail.com", "raghavdevgon@yahoo.com"]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.independent.co.uk/register?regSourceMethod=login+overlay")
     p1 = driver.page_source
@@ -32,4 +32,4 @@ for i in ["supeasdasdrman@gmail.com", "dahopi6913@dwgtcm.com", "priyanka@gmail.c
     if "Email already in use. Log in or try another" in p3:
         print(i)
     sleep(2)
-driver.close()
+    driver.close()
