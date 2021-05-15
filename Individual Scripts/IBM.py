826d1f61cea6979d83eb61c335d8ec08e920263a
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:46:10 -0400

    Update IBM.py

--- Individual Scripts/IBM.py
+++ Individual Scripts/IBM.py
@@ -4,12 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
 
 print("The usernames present in IBM are:")
 
@@ -17,6 +11,13 @@ print("The usernames present in IBM are:")
 # You can remove all the Email IDs below
 for i in ["kjanjkwdd@yahoo.com", "priyanka@gmail.com", "batman@gmail.com", "michael@gmail.com", "raghavdevgon@yahoo.com"]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.ibm.com/account/reg/us-en/signup?formid=urx-19776&target=https%3A%2F%2Flogin.ibm.com%2Foidc%2Fendpoint%2Fdefault%2Fauthorize%3FqsId%3De93ce9cd-59c0-4ecd-82dd-cec7b05a83bf%26client_id%3Dv18LoginProdCI")
     p1 = driver.page_source
@@ -31,4 +32,4 @@ for i in ["kjanjkwdd@yahoo.com", "priyanka@gmail.com", "batman@gmail.com", "mich
     if "This email is associated with an IBM account" in p3:
         print(i)
     sleep(2)
-driver.close()
+    driver.close()
