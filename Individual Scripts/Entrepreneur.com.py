371809ea97bb6f58a7d81bd3eb954f3f6967eee3
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:39:52 -0400

    Update Entrepreneur.com.py

--- Individual Scripts/Entrepreneur.com.py
+++ Individual Scripts/Entrepreneur.com.py
@@ -4,19 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in Entrepreneur are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in ["superman@gmail.com", "yenojam209@shzsedu.com", "priyasdsdanka@gmail.com", "batman@gmail.com", "asjdbkajsdnkjas@gmail.com", "spiderman@gmail.com"]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.entrepreneur.com/dashboard/forgot")
     p1 = driver.page_source
@@ -31,4 +31,4 @@ for i in ["superman@gmail.com", "yenojam209@shzsedu.com", "priyasdsdanka@gmail.c
     if "We just sent you an email with instructions to reset your password" in p3:
         print(i)
     sleep(2)
-driver.close()
+    driver.close()
