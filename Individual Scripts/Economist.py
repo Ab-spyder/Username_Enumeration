91e2cfa6496dcf7d5ca5701607e862453ce5a4de
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:39:48 -0400

    Update Economist.py

--- Individual Scripts/Economist.py
+++ Individual Scripts/Economist.py
@@ -4,18 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
 
 print("The usernames present in Economist are:")
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in [ "priyanka@gmail.com", "batman@gmail.com", "michael@gmail.com", "raghavdevgon@yahoo.com"]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://myaccount.economist.com/s/authentication?starturl=https%3A%2F%2Fsubscribenow.economist.com%2Fcheckout")
     p1 = driver.page_source
@@ -31,4 +32,4 @@ for i in [ "priyanka@gmail.com", "batman@gmail.com", "michael@gmail.com", "ragha
     if "You already have a registered account." in p3:
         print(i)
     sleep(2)
-driver.close()
+    driver.close()
