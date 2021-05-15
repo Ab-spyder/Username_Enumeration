2ab88e6679994ccaf639d5909e82c2ff46b6982e
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:39:58 -0400

    Update Evernote.py

--- Individual Scripts/Evernote.py
+++ Individual Scripts/Evernote.py
@@ -4,13 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in Evernote are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
@@ -18,6 +11,13 @@ print("The usernames present in Evernote are:")
 for i in ["ccohen@aol.com", "superman@gmail.com", "keijser@aol.com", "maryjane@gmail.com", "ajohnson@hotmail.com", "rnewman@aol.com", "sfoskett@hotmail.com","seurat@aol.com", "richard@gmail.com", "punkis@gmail.com", "jadavis@outlook.com", "staffelb@att.net","pplinux@icloud.com", "lamky@optonline.net", "ccoerhen@aol.com", "keijserqer@aol.com","ajohneesdson@hotmail.com", "rnewman@aol.com", "sfosksdett@hotmail.com", "seusdrat@aol.com","richard@gmail.com", "pssunkis@gmail.com", "jadavssis@outlook.com", "stafssfelb@att.net", "pplinsdux@icloud.com", "laamky@optonline.net"]:
 
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.evernote.com/Registration.action?analyticsLoginOrigin=login_action&clipperFlow=false&showSwitchService=true&usernameImmutable=false&_sourcePage=TowVCA5XYVniMUD9T65RG_YvRLZ-1eYO3fqfqRu0fynRL_1nukNa4gH1t86pc1SP&__fp=s6jlGm5AtnM%3D")
     p1 = driver.page_source
@@ -32,4 +32,4 @@ for i in ["ccohen@aol.com", "superman@gmail.com", "keijser@aol.com", "maryjane@g
 
     if "This email address is already in use." in p3:
         print(i)
-driver.close()
+    driver.close()
