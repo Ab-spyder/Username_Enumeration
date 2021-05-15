b9ca821c8ad924cda4db1ba85562ac7fa6f547fd
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:56:21 -0400

    Update Webs.com .py

--- Individual Scripts/Webs.com .py
+++ Individual Scripts/Webs.com .py
@@ -4,19 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in Webs are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in ["supeasdasdrman@gmail.com", "priyanka@gmail.com", "batman@gmail.com", "michael@gmail.com", "raghavdevgon@yahoo.com"]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.webs.com/s/login/forgotPassword")
     p1 = driver.page_source
@@ -33,4 +33,4 @@ for i in ["supeasdasdrman@gmail.com", "priyanka@gmail.com", "batman@gmail.com", 
     if "We've sent password reset instructions to your email address." in p2:
         print(i)
     sleep(2)
-driver.close()
+    driver.close()
