382a4a9a6a59be0120da6457b987577fd1b18bc4
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:45:53 -0400

    Update Fastmail.py

--- Individual Scripts/Fastmail.py
+++ Individual Scripts/Fastmail.py
@@ -5,19 +5,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in GMX Mail are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in ["superman", "BegForMercy", "batman", "BraiasjdnkjnAxe", "CrazyMind" ]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.fastmail.com/signup/")
     sleep(3)
@@ -31,7 +31,7 @@ for i in ["superman", "BegForMercy", "batman", "BraiasjdnkjnAxe", "CrazyMind" ]:
     p3 = extract_html_diff.as_string(p2, p1)
     if "has already been taken." in p3:
        print(i)
-driver.close()
+    driver.close()
 
 
 
