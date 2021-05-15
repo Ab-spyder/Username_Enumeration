97efcc4e2f88e00751f50c3d176817273e3e9532
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:56:18 -0400

    Update Wiley.py

--- Individual Scripts/Wiley.py
+++ Individual Scripts/Wiley.py
@@ -4,19 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in Wiley are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in ["superman@gmail.com", "priyanka@gmail.com", "djbjasdtman@gmail.com", "michael@gmail.com", "raghavdevgon@yahoo.com"]:
     user = i
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.wiley.com/en-us/login/checkout")
     p1 = driver.page_source
@@ -30,4 +30,4 @@ for i in ["superman@gmail.com", "priyanka@gmail.com", "djbjasdtman@gmail.com", "
     if "! An account already exists for this email address. Please log in." in p3:
         print(i)
     sleep(2)
-driver.close()
+    driver.close()
