65b8073145e8b962f2e23733fd8d5c29c7167e8c
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:56:25 -0400

    Update Twitch.py

--- Individual Scripts/Twitch.py
+++ Individual Scripts/Twitch.py
@@ -5,13 +5,6 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
-
 print("The usernames present in Twitch are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
@@ -20,6 +13,12 @@ for i in ["BegForMercy", "BraiasjdnkjnAxe", "CrazyMind", "DeathWaasdaish", "Disa
 
     user = i
 
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     driver.get("https://www.twitch.tv/")
     element = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/nav/div/div[3]/div[3]/div/div[1]/div[2]/button/div/div')
@@ -32,4 +31,4 @@ for i in ["BegForMercy", "BraiasjdnkjnAxe", "CrazyMind", "DeathWaasdaish", "Disa
     p1 = driver.page_source
     if "*This username is unavailable." in p1:
         print(user)
-driver.close()
+    driver.close()
