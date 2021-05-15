3f6aef923350552e727fb1e19769d73ffb226d02
ningala-a <74433590+ningala-a@users.noreply.github.com>
2021-05-14 23:39:55 -0400

    Update Eventbrite.py

--- Individual Scripts/Eventbrite.py
+++ Individual Scripts/Eventbrite.py
@@ -4,18 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import extract_html_diff
 
-# Using edge browser
-driver = webdriver.Edge(executable_path='msedgedriver')
-
-# Using Firefox browser
-#driver = webdriver.FirefoxProfile()
-#driver = webdriver.Firefox(driver)
 
 print("The usernames present in Eventbrite are:")
 
 # Place all the Email IDs to test in the for loop as shown below.
 # You can remove all the Email IDs below
 for i in ["spider1@gmail.com", "superman@gmail.com", "michael@gmail.com", "wuasdbaabba@gmail.com"]:
+
+    # Using edge browser
+    driver = webdriver.Edge(executable_path='msedgedriver')
+
+    # Using Firefox browser
+    # driver = webdriver.FirefoxProfile()
+    # driver = webdriver.Firefox(driver)
 
     user = i
     passw = 'wjnjj!@3221j' # sample password to enter to the input box
@@ -35,4 +36,4 @@ for i in ["spider1@gmail.com", "superman@gmail.com", "michael@gmail.com", "wuasd
     if "The password is not correct." in p3:
         print(i)
     sleep(2)
-driver.close()
+    driver.close()
